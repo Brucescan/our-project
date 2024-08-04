0ad0ab5d905783e9d2726418dd64d193f6464116
SULANGREN\sulangren <1503663813@qq.com>
2024-08-04 17:32:31 +0800

    数据库链接

--- Cfs/Cfs/settings.py
+++ Cfs/Cfs/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'Cfs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '787866554',
+        'NAME': 'project',
     }
 }
 
