187f61fd35cca3be4246a47d3bd353526f97e35f
Brucescan <1637252023@qq.com>
2024-08-05 12:03:35 +0800

    system-index-start

--- Cfs/Cfs/urls.py
+++ Cfs/Cfs/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from Cfs import views
 
 urlpatterns = [
-    path('', include('system.urls'))
+    # 项目的根目录
+    path('', views.index)
 ]
